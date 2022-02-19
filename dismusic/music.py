331066a3066cbed7c2d891e481d09f4f259b13b1
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-02-19 07:51:00 -0800

    Update music.py

--- dismusic/music.py
+++ dismusic/music.py
@@ -97,7 +97,7 @@ class Music(commands.Cog):
         player.bound_channel = ctx.channel
         player.bot = self.bot
 
-        await ctx.send(f"**Connected to **`{player.channel.name}`")
+        await interaction.message.edit(content=f"**Connected to **`{player.channel.name}`")
 
     @slash_command(aliases=["vol"])
     @voice_channel_player()
