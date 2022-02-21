330a3a044d7b664608ba2422acb786b603fec63f
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-02-20 23:19:33 -0800

    Update and rename dismusic/__init__.py to cord-music/__init__.py

--- cord-music/__init__.py
+++ cord-music/__init__.py
@@ -1,4 +1,4 @@
-"""Copyright 2022 Md Shahriyar Alam
+"""Copyright 2022 NixonXC
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
 to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
