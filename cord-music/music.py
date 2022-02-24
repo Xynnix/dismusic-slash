068c1c8093ac40533e08f215eebd9a7191c6db3d
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-02-24 07:58:34 -0800

    Update music.py

--- cord-music/music.py
+++ cord-music/music.py
@@ -97,7 +97,7 @@ class Music(commands.Cog):
         player.bound_channel = ctx.channel
         player.bot = self.bot
 
-        await msg.response.edit_message(content=f"**Connected to **`{player.channel.name}`")
+        await msg.response.edit_original_message(content=f"**Connected to **`{player.channel.name}`")
 
     @slash_command(aliases=["vol"])
     @voice_channel_player()
