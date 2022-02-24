446b6a192b64742086f016191529749d8af7e8b9
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-02-24 08:25:47 -0800

    Update music.py

--- cord-music/music.py
+++ cord-music/music.py
@@ -60,7 +60,7 @@ class Music(commands.Cog):
 
         track = tracks[0]
 
-        await msg.edit_original_message(content=f"**Added** `{track.title}` **to queue.** ")
+        await ctx.send(f"**Added** `{track.title}` **to queue.** ")
         await player.queue.put(track)
 
         if not player.is_playing():
