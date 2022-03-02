136f12b3a7370a26a5210d0141114d5f1846bdd0
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-03-02 09:25:15 -0800

    Update player.py

--- cord-music/player.py
+++ cord-music/player.py
@@ -87,6 +87,29 @@ class DisPlayer(Player):
         )
         embed.add_field(name="Looping", value=self.loop)
         embed.add_field(name="Volume", value=self.volume)
+        b4 = Button(label="Stop", emoji="⏹")
+        b3 = Button(label="Skip", emoji="⏭")
+        async def b3_callback(interaction):
+            player: DisPlayer = ctx.voice_client
+
+            if player.loop == "CURRENT":
+                player.loop = "NONE"
+
+            await player.stop()
+
+            self.bot.dispatch("dismusic_track_skip", player)
+            await interaction.response.send_message("**Skipped** :track_next:", ephemeral=True)
+        b3.callback = b3_callback
+        async def b4_callback(interaction):
+            player: DisPlayer = ctx.voice_client
+
+            await player.destroy()
+            await interaction.response.send_message("**Stopped the player** :stop_button:", ephemeral=True)
+            self.bot.dispatch("dismusic_player_stop", player)
+        b4.callback = b4_callback
+        view = View()
+        view.add_item(b4)
+        view.add_item(b3)
 
         next_song = ""
 
@@ -100,5 +123,5 @@ class DisPlayer(Player):
             embed.add_field(name="Next Song", value=next_song, inline=False)
 
         if not ctx:
-            return await self.bound_channel.send(embed=embed)
-        await ctx.send(embed=embed)
+            return await self.bound_channel.send(embed=embed, view=view)
+        await ctx.send(embed=embed, view=view)
