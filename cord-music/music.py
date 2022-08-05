3d7216dfbe659ff4f7fabb63ce8f550312d87339
NixonXC <invasionyt19@gmail.com>
2022-08-05 04:54:46 -0700

    Update music.py

--- cord-music/music.py
+++ cord-music/music.py
@@ -124,7 +124,7 @@ class Music(commands.Cog):
     @voice_connected()
     async def play(self, ctx: commands.Context, *, query: str):
         """Play or add song to queue (Defaults to YouTube)"""
-        await ctx.invoke(self.connect, ctx)
+        await ctx.invoke(self.connect)
         await self.play_track(ctx, query)
         
     @slash_command(aliases=["con"])
